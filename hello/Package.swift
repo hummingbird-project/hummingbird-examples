2f238468545230b5170978f36921f667be82a58b
Adam Fowler <adamfowler71@gmail.com>
2021-01-30 15:37:23 +0000

    Add -cross-module-optimization on release builds

--- hello/Package.swift
+++ hello/Package.swift
@@ -13,9 +13,17 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0")
     ],
     targets: [
-        .target(name: "hummingbird-hello", dependencies: [
-            .product(name: "Hummingbird", package: "hummingbird"),
-            .product(name: "ArgumentParser", package: "swift-argument-parser")
-        ]),
+        .target(name: "hummingbird-hello",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
     ]
 )
