dc8ff4d03fb31ac632e095e08de693af9744e81e
Adam Fowler <adamfowler71@gmail.com>
2021-02-04 16:26:00 +0000

    Apply headers to request response

--- todos/Sources/hummingbird-todos/Middleware/CORSMiddleware.swift
+++ todos/Sources/hummingbird-todos/Middleware/CORSMiddleware.swift
@@ -8,13 +8,13 @@ struct CORSMiddleware: HBMiddleware {
                 "access-control-allow-headers": "content-type",
                 "access-control-allow-methods": "OPTIONS, GET, HEAD, POST"
             ]
-            return request.success(HBResponse(status: .noContent, headers: headers, body: .empty)).map { response in
+            return request.success(HBResponse(status: .noContent, headers: headers, body: .empty))
+        } else {
+            return next.respond(to: request).map { response in
                 response.headers.replaceOrAdd(name: "access-control-allow-origin", value: "*")
                 response.headers.replaceOrAdd(name: "access-control-allow-headers", value: "content-type")
                 return response
             }
-        } else {
-            return next.respond(to: request)
         }
     }
 }
