e08debec1f13bfaa87320eb4d257bbf21adceeb7
Adam Fowler <adamfowler71@gmail.com>
2021-02-17 19:07:45 +0000

    Remove local version of HummingbirdLambda

--- todos-lambda/Package.swift
+++ todos-lambda/Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .executable(name: "HummingbirdTodosLambda", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime", from: "0.2.0"),
-        .package(url: "https://github.com/swift-extras/swift-extras-base64.git", from: "0.5.0"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "0.2.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.2.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-lambda.git", from: "0.1.1"),
         .package(url: "https://github.com/soto-project/soto.git", from: "5.0.0"),
     ],
     targets: [
@@ -21,19 +21,14 @@ let package = Package(
             .byName(name: "App")
         ]),
         .target(name: "App", dependencies: [
-            .byName(name: "HummingbirdLambda"),
             .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
             .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
             .product(name: "Hummingbird", package: "hummingbird"),
             .product(name: "HummingbirdFoundation", package: "hummingbird"),
+            .product(name: "HummingbirdLambda", package: "hummingbird-lambda"),
             .product(name: "SotoDynamoDB", package: "soto")
-        ]),
-        .target(name: "HummingbirdLambda", dependencies: [
-            .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-            .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
-            .product(name: "ExtrasBase64", package: "swift-extras-base64"),
-            .product(name: "Hummingbird", package: "hummingbird")
         ]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
+
