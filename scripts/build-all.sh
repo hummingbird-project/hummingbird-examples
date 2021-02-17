5f3f16bac9faeaa9acbd00c4645d8b24896c7ee8
Adam Fowler <adamfowler71@gmail.com>
2021-02-17 18:56:23 +0000

    Add todos-lambda and session-redis to build-all script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -17,5 +17,7 @@ build_example()
 build_example hello
 build_example html-form
 build_example http2
+build_example session-redis
 build_example todos-dynamodb
 build_example todos-fluent
+build_example todos-lambda
