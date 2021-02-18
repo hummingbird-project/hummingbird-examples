297af99df13963e6a2dc8fc1b284787223081621
Adam Fowler <adamfowler71@gmail.com>
2021-02-18 10:34:23 +0000

    Remove addFoundation

--- hello/Sources/hummingbird-hello/app.swift
+++ hello/Sources/hummingbird-hello/app.swift
@@ -3,7 +3,6 @@ import HummingbirdFoundation
 
 func runApp(_ arguments: HummingbirdArguments) {
     let app = HBApplication(configuration: .init(address: .hostname(arguments.hostname, port: arguments.port)))
-    app.addFoundation()
     app.router.get("/") { _ in
         return "Hello"
     }
