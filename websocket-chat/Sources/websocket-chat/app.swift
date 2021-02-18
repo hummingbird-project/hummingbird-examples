7e7d7c0d130d1ec181df29d6ee77592355bcb9f4
Adam Fowler <adamfowler71@gmail.com>
2021-02-18 13:19:27 +0000

    comments

--- websocket-chat/Sources/websocket-chat/app.swift
+++ websocket-chat/Sources/websocket-chat/app.swift
@@ -11,24 +11,29 @@ func runApp(_ arguments: HummingbirdArguments) {
     app.ws.on(
         "/chat",
         shouldUpgrade: { request in
+            // only allow upgrade if username query parameter exists
             guard request.uri.queryParameters["username"] != nil else {
                 return request.failure(.badRequest)
             }
             return request.success([:])
         },
         onUpgrade: { request, ws in
+            // close websocket if no username parameter
             guard let username = request.uri.queryParameters["username"] else {
                 ws.close(promise: nil)
                 throw HBHTTPError(.badRequest)
             }
+            // if username is already connected output this info and close the connection
             guard request.application.connectionMgr.get(name: String(username)) == nil else {
                 ws.write(.text("\(username) is already connected"))
                 ws.close(promise: nil)
                 return
             }
+            // add new user
             request.application.connectionMgr.newUser(name: String(username), ws: ws)
         }
     )
+    // initialize connection manager
     app.connectionMgr = .init()
     
     app.start()
