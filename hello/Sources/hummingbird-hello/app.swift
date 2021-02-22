09c9ad092375a19411c7225b8be89508e4a7cf07
Adam Fowler <adamfowler71@gmail.com>
2021-02-22 17:38:44 +0000

    Add Filemiddleware to hello

--- hello/Sources/hummingbird-hello/app.swift
+++ hello/Sources/hummingbird-hello/app.swift
@@ -1,7 +1,9 @@
 import Hummingbird
+import HummingbirdFoundation
 
 func runApp(_ arguments: HummingbirdArguments) {
     let app = HBApplication(configuration: .init(address: .hostname(arguments.hostname, port: arguments.port)))
+    app.middleware.add(HBFileMiddleware(application: app))
     app.router.get("/") { _ in
         return "Hello"
     }
