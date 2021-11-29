8bdb16d57eea61d76d87bb28c8c593e10b1344b2
Michael Critz <github@michaelcritz.com>
2021-11-29 08:16:38 -0800

    adds draft file upload example (#19)
    * adds draft file upload example
    
    * Finished example. Added test.
    
    * Adds download, addresses PR feedback
    
    * Update README.md
    
    * Update FileController.swift
    
    Addresses PR Feedback

---
+++ upload-async/Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "upload-async",
+    platforms: [.macOS("12.0")],
+    products: [
+        .executable(name: "Server", targets: ["Server"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.14.3"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0")
+    ],
+    targets: [
+        .target(name: "App",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "HummingbirdFoundation", package: "hummingbird"),
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .executableTarget(name: "Server",
+            dependencies: [
+                .byName(name: "App"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
+        .testTarget(name: "AppTests",
+            dependencies: [
+                .byName(name: "App"),
+                .product(name: "HummingbirdXCT", package: "hummingbird")
+            ]
+        )
+    ]
+)
---
+++ upload-async/Tests/AppTests/AppTests.swift
@@ -0,0 +1,50 @@
+@testable import App
+import Hummingbird
+import HummingbirdXCT
+import XCTest
+
+final class AppTests: XCTestCase {
+    func testApp() throws {
+        let app = HBApplication(testing: .live)
+        try app.configure()
+
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        let textString = "Hello, World!"
+        let testFileName = "Hello.txt"
+        let testUpload = UploadModel(filename: testFileName)
+        let uploadURL = try testUpload.destinationURL(allowsOverwrite: true)
+        defer {
+            try? FileManager.default.removeItem(at: uploadURL)
+        }
+        let buffer = ByteBufferAllocator().buffer(string: textString)
+
+        app.XCTExecute(uri: "/files",
+                       method: .POST,
+                       headers: ["File-Name" : testFileName],
+                       body: buffer) { response in
+            XCTAssertEqual(response.status, .ok)
+            guard let body = response.body else {
+                XCTFail("Response should contain a valid body")
+                return
+            }
+            XCTAssertTrue(body.contains(string: testFileName))
+        }
+        
+        app.XCTExecute(uri: "/files/\(testFileName)", method: .GET) { response in
+            guard let body = response.body else {
+                XCTFail("Response should contain a valid body")
+                return
+            }
+            let downloadString = String(buffer: body)
+            XCTAssertEqual(downloadString, textString, "Downloaded bytes should match uploaded bytes")
+        }
+    }
+}
+
+fileprivate extension ByteBuffer {
+    func contains(string: String) -> Bool {
+        return String(buffer: self).contains(string)
+    }
+}
