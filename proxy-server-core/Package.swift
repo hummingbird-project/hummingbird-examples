5f1a1ec4e8563727b0c9926f32001b9db43c39c3
Adam Fowler <adamfowler71@gmail.com>
2021-11-30 11:19:50 +0000

    Proxy server example (#20)
    * Proxy server example
    
    I've hacked the streaming of response together, need to revisit code in HummingbirdCore to tidy this up
    
    * Fix proxy server example after changes
    
    * Add tests
    
    * Some comments
    
    * Read contentLength from  request
    
    * Remove host from headers, so it is updated
    
    * Update README.md
    
    * Update README.md
    
    * Move proxy-server to proxy-server-core
    
    * proxy server using Middleware
    
    * Add ability to map location
    
    * Update README
    
    * Update main readme with links

---
+++ proxy-server-core/Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "proxy-server-core",
+    platforms: [.macOS(.v10_14)],
+    products: [
+        .executable(name: "Server", targets: ["Server"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", .upToNextMinor(from: "0.13.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.6.0"),
+    ],
+    targets: [
+        .target(name: "App",
+            dependencies: [
+                .product(name: "HummingbirdCore", package: "hummingbird-core"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .target(name: "Server",
+            dependencies: [
+                .byName(name: "App"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
+        .testTarget(name: "AppTests",
+            dependencies: [
+                .byName(name: "App"),
+                .product(name: "HummingbirdCoreXCT", package: "hummingbird-core")
+            ]
+        )
+    ]
+)
---
+++ proxy-server-core/Sources/App/ProxyServer.swift
@@ -0,0 +1,76 @@
+import AsyncHTTPClient
+import HummingbirdCore
+import Logging
+import NIO
+import NIOHTTP1
+import Network
+
+public struct HTTPProxyServer: HBHTTPResponder {
+    enum ProxyError: Error {
+        case invalidURL
+    }
+    let targetServer: String
+    let httpClient: HTTPClient
+    public let logger: Logger
+    
+    public init(targetServer: String, httpClient: HTTPClient, logger: Logger) {
+        self.targetServer = targetServer
+        self.httpClient = httpClient
+        self.logger = logger
+    }
+
+    public func respond(to request: HBHTTPRequest, context: ChannelHandlerContext, onComplete: @escaping (Result<HBHTTPResponse, Error>) -> Void) {
+        logger.info("\(request.head.uri)")
+        do {
+            // create request
+            let request = try request.ahcRequest(host: targetServer, eventLoop: context.eventLoop)
+            // create response body streamer
+            let streamer = HBByteBufferStreamer(eventLoop: context.eventLoop, maxSize: 2048*1024)
+            // delegate for streaming bytebuffers from AsyncHTTPClient
+            let delegate = StreamingResponseDelegate(on: context.eventLoop, streamer: streamer)
+            // execute request
+            _ = httpClient.execute(
+                request: request,
+                delegate: delegate,
+                eventLoop: .delegateAndChannel(on: context.eventLoop),
+                logger: logger
+            )
+            // when delegate receives header then single completion
+            delegate.responsePromise.futureResult.whenComplete { result in
+                onComplete(result)
+            }
+        } catch {
+            onComplete(.failure(ProxyError.invalidURL))
+        }
+    }
+}
+
+extension HBHTTPRequest {
+    /// create AsyncHTTPClient request from Hummingbird Request
+    func ahcRequest(host: String, eventLoop: EventLoop) throws -> HTTPClient.Request {
+        var headers = self.head.headers
+        headers.remove(name: "host")
+        switch self.body {
+        case .byteBuffer(let buffer):
+            return try HTTPClient.Request(
+                url: host + self.head.uri,
+                method: self.head.method,
+                headers: headers,
+                body: buffer.map { .byteBuffer($0) }
+            )
+
+        case .stream(let stream):
+            let contentLength = self.head.headers["content-length"].first.map { Int($0) } ?? nil
+            return try HTTPClient.Request(
+                url: host + self.head.uri,
+                method: self.head.method,
+                headers: headers,
+                body: .stream(length: contentLength) { writer in
+                    return stream.consumeAll(on: eventLoop) { byteBuffer in
+                        writer.write(.byteBuffer(byteBuffer))
+                    }
+                }
+            )
+        }
+    }
+}
