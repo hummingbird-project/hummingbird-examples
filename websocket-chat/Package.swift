3527e24177ed3fddcaafec8b64449fd0a99d0df5
Adam Fowler <adamfowler71@gmail.com>
2023-02-16 10:32:43 +0100

    Fix websocket-chat Package.swift

--- websocket-chat/Package.swift
+++ websocket-chat/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "1.0.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", from: "1.0.0.2"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
     ],
     targets: [
