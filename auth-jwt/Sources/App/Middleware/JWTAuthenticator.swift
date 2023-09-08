0c6628186c83e39486c38168b8af2121956a14f6
Adam Fowler <adamfowler71@gmail.com>
2023-09-08 11:48:25 +0100

    authenticator should store signer

--- auth-jwt/Sources/App/Middleware/JWTAuthenticator.swift
+++ auth-jwt/Sources/App/Middleware/JWTAuthenticator.swift
@@ -5,38 +5,43 @@ import JWTKit
 import NIOFoundationCompat
 
 struct JWTPayloadData: JWTPayload, Equatable, HBAuthenticatable {
-  enum CodingKeys: String, CodingKey {
-    case subject = "sub"
-    case expiration = "exp"
-  }
+    enum CodingKeys: String, CodingKey {
+        case subject = "sub"
+        case expiration = "exp"
+    }
 
-  var subject: SubjectClaim
-  var expiration: ExpirationClaim
-  // Define additional JWT Attributes here
+    var subject: SubjectClaim
+    var expiration: ExpirationClaim
+    // Define additional JWT Attributes here
 
-  func verify(using signer: JWTSigner) throws {
-    try self.expiration.verifyNotExpired()
-  }
+    func verify(using signer: JWTSigner) throws {
+        try self.expiration.verifyNotExpired()
+    }
 }
 
 struct JWTAuthenticator: HBAsyncAuthenticator {
-  var jwks: JWKS
+    let jwtSigners: JWTSigners
 
-  init(jwksData: ByteBuffer) throws {
-    self.jwks = try JSONDecoder().decode(JWKS.self, from: jwksData)
-  }
-
-  func authenticate(request: HBRequest) async throws -> JWTPayloadData? {
-    guard let jwtToken = request.authBearer?.token else { throw HBHTTPError(.unauthorized) }
-
-    let signers = JWTSigners()
-    do {
-      try signers.use(jwks: self.jwks)
-      let payload = try signers.verify(jwtToken, as: JWTPayloadData.self)
-      return payload
-    } catch {
-      request.logger.debug("couldn't verify token")
-      throw HBHTTPError(.unauthorized)
+    init(jwksData: ByteBuffer) throws {
+        let jwks = try JSONDecoder().decode(JWKS.self, from: jwksData)
+        self.jwtSigners = JWTSigners()
+        try self.jwtSigners.use(jwks: jwks)
     }
-  }
+
+    init(_ signer: JWTSigner, kid: JWKIdentifier? = nil) throws {
+        self.jwtSigners = JWTSigners()
+        self.jwtSigners.use(signer, kid: kid)
+    }
+
+    func authenticate(request: HBRequest) async throws -> JWTPayloadData? {
+        guard let jwtToken = request.authBearer?.token else { throw HBHTTPError(.unauthorized) }
+
+        do {
+            let payload = try self.jwtSigners.verify(jwtToken, as: JWTPayloadData.self)
+            return payload
+        } catch {
+            request.logger.debug("couldn't verify token")
+            throw HBHTTPError(.unauthorized)
+        }
+    }
 }
