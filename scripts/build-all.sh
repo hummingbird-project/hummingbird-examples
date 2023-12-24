01eece57d6c0724c0541a66315de6dab55d3a68b
Adam Fowler <adamfowler71@gmail.com>
2023-12-24 13:54:10 +0100

    Only build updated examples (#45)
    * Run Build on only updated examples
    
    * Remove -r from git call
    
    * Compare against base ref
    
    * Use FETCH_HEAD
    
    * Add setting of both branches
    
    * trigger on all paths being set
    
    * Use GITHUB_HEAD_REF
    
    * Update build-all.sh
    
    * Update ci.yml
    
    * Fix build-all.sh
    
    * Mark workspace as safe
    
    * Cleanup
    
    * Use set-safe-directory
    
    * Fetch all
    
    * don;t use action/checkout set-safe-workspace

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+usage()
+{
+    echo "Usage: build-all.sh -u"
+    exit 2
+}
 
 build_example()
 {
@@ -25,12 +31,41 @@ pushd "$here"/..
 # folders to ignore
 ignore_list="scripts ios-image-server"
 
+COMPARISON=""
+DRY_RUN=""
+
+while getopts 'du:' option
+do
+    case $option in
+        u) COMPARISON=$OPTARG ;;
+        d) DRY_RUN=1 ;;
+        *) usage ;;
+    esac
+done
+
 # get list of folders and remove ignore list
-folders=$(find * -maxdepth 0 -type d)
+if [[ -n "$COMPARISON" ]]; then
+    COMPARE1=$(echo "$COMPARISON" | sed s/:.\*$//)
+    COMPARE2=$(echo "$COMPARISON" | sed s/^.\*://)
+    if [[ "$COMPARE1" == "$COMPARE2" ]]; then
+        COMPARE1=""
+    fi
+    echo "Comparing $COMPARE1 with $COMPARE2"
+    # get intersection between folders at root level and list of folders that have changed in merge commit
+    folders=$(comm -12 <(find * -maxdepth 0 -type d) <(git --no-pager diff --name-only $COMPARE1 "$COMPARE2" | awk -F "/" '{print $1}' | sort -u))
+else
+    folders=$(find * -maxdepth 0 -type d)
+fi
 
 for i in $ignore_list; do
     folders=$(echo "$folders" | sed "s/$i//g")
 done
+
+echo "Updating:"
+echo "$folders"
+if [[ -n "$DRY_RUN" ]]; then
+    exit 0
+fi
 
 # Test latest code against examples
 for f in $folders; do
