b7a5f6b669746bbc881b4d66a2cdf71a5ac43297
Adam Fowler <adamfowler71@gmail.com>
2024-01-09 08:07:45 +0100

    Clean up PostgresClientService

--- todos-postgres-tutuorial/Sources/PostgresClientService.swift
+++ todos-postgres-tutuorial/Sources/PostgresClientService.swift
@@ -3,22 +3,12 @@ import Hummingbird
 import ServiceLifecycle
 
 /// Manage the lifecycle of a PostgresClient
-actor PostgresClientService: Service {
+struct PostgresClientService: Service {
     let client: PostgresClient
 
-    init(client: PostgresClient) {
-        self.client = client
-    }
-
-    func run() async throws {
-        await withTaskGroup(of: Void.self) { group in
-            group.addTask {
-                await self.client.run()
-            }
-            // wait until graceful shutdown and then cancel all tasks
-            await GracefulShutdownWaiter().wait()
-            group.cancelAll()
-            print("Shutdown postgres")
+    func run() async {
+        await cancelOnGracefulShutdown {
+            await self.client.run()
         }
     }
 }
