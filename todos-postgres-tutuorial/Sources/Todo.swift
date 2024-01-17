3305e232c1fbdf42185bbab9066e172a9abf78f9
Adam Fowler <adamfowler71@gmail.com>
2024-01-17 10:16:23 +0000

    2.x.x todos tutorial (#47)
    * Todos tutorial up to testing
    
    * Postgres
    
    * Clean up
    
    * Tried to clean up patch.
    
    Otherwise variable rename, comment updates
    
    * Move todos-tutorial to todos-postgres-tutorial
    
    * Repository not Respository
    
    * Patch parameter pack interpolation
    
    * No point keeping todos-postgres if we have the tutorial
    
    * Clean up PostgresClientService
    
    * Updates since changes to Hummingbird
    
    * Simplified update, Updates from PR comments
    
    * Fixed patch test

---
+++ todos-postgres-tutuorial/Sources/Todo.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Hummingbird
+
+
+struct Todo {
+    // Todo ID
+    var id: UUID
+    // Title 
+    var title: String
+    // Order number
+    var order: Int?
+    // URL to get this ToDo
+    var url: String
+    // Is Todo complete
+    var completed: Bool?
+}
+
+
+extension Todo: HBResponseEncodable, Decodable, Equatable {}
---
+++ todos-postgres-tutuorial/Sources/TodoRepository.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+/// Interface for storing and editing todos
+protocol TodoRepository {
+    /// Create todo.
+    func create(title: String, order: Int?, urlPrefix: String) async throws -> Todo
+    /// Get todo
+    func get(id: UUID) async throws -> Todo?
+    /// List all todos
+    func list() async throws -> [Todo]
+    /// Update todo. Returns updated todo if successful
+    func update(id: UUID, title: String?, order: Int?, completed: Bool?) async throws -> Todo?
+    /// Delete todo. Returns true if successful
+    func delete(id: UUID) async throws -> Bool
+    /// Delete all todos
+    func deleteAll() async throws
+}
