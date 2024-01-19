5d6b5b5436e93dbbac0b8ef9232a4707b38896d4
Adam Fowler <adamfowler71@gmail.com>
2024-01-19 07:54:58 +0000

    Add some comments

--- todos-postgres-tutuorial/Sources/TodoRequestContext.swift
+++ todos-postgres-tutuorial/Sources/TodoRequestContext.swift
@@ -5,7 +5,9 @@ import Logging
 /// Custom request context setting up JSON decoding and encoding
 struct TodoRequestContext: HBRequestContext {
     var coreContext: HBCoreRequestContext
+    /// Set request decoder to be JSONDecoder
     var requestDecoder: JSONDecoder { .init() }
+    /// Set response encoder to be JSONEncdoer
     var responseEncoder: JSONEncoder { .init() }
 
     init(allocator: ByteBufferAllocator, logger: Logger) {
