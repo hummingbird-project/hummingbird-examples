6ab0ee080c7a3831754e4a91fc4877ff0ab6cd57
Adam Fowler <adamfowler71@gmail.com>
2024-01-22 16:41:10 +0000

    Update examples to use HB 2.0.0-alpha.1

--- todos-postgres-tutuorial/Package.swift
+++ todos-postgres-tutuorial/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.macOS(.v14)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "2.x.x"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-alpha.1"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.20.0"),
     ],
     targets: [
--- upload/Package.swift
+++ upload/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "upload-async",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "2.x.x"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-alpha.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
     ],
     targets: [
