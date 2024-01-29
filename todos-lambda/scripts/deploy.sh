25ddaa6d8c60b91a5a8b8f82cc1e29d4250d9fd2
Adam Fowler <adamfowler71@gmail.com>
2024-01-29 17:47:50 +0000

    Update deploy.sh to ensure user is running on correct architecture

--- todos-lambda/scripts/deploy.sh
+++ todos-lambda/scripts/deploy.sh
@@ -15,11 +15,16 @@
 
 here=$(dirname "$0")
 
-if [ ! $(which sam) ]; then
+if [ ! "$(which sam)" ]; then
     echo "The deploy script requires AWS SAM."
     echo "More information about AWS SAM and installation instructions can be found at https://aws.amazon.com/serverless/sam/"
-    exit -1
+    exit 255
 fi
 
-cd "$here"/..
+if [ "$(uname -m)" != "arm64" ]; then
+  echo "Error: SAM configuration has arm64 architecture. Please edit scripts/deploy.sh and scripts/sam.yml to set the correct architecture (x86_64)."
+  exit 1
+fi
+
+cd "$here"/.. || exit
 sam deploy --stack-name hb-todos-lambda --resolve-s3 --template scripts/sam.yml $@
