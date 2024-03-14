07033bdb6c46b88ec714fd8d25dbde3fa0bed212
Adam Fowler <adamfowler71@gmail.com>
2024-03-14 11:25:36 +0000

    Use 2.0 beta

--- jobs/Package.swift
+++ jobs/Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "jobs",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "main"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-redis.git", branch: "main"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-redis.git", from: "2.0.0-beta.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     ],
     targets: [
