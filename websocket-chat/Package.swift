d2ac020edf8bb58203061b49673dd6455094f29d
Adam Fowler <adamfowler71@gmail.com>
2024-04-19 08:24:47 +0100

    Update websockets

--- websocket-chat/Package.swift
+++ websocket-chat/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-beta"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", branch: "main"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", from: "2.0.0-alpha.3"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     ],
     targets: [
