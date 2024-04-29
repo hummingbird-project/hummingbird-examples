30a5dc0dc0d94e82b4e6bda119c5064da733db72
Adam Fowler <adamfowler71@gmail.com>
2024-04-29 12:25:28 +0100

    Fix htmlform tests

--- html-form/Tests/AppTests/AppTests.swift
+++ html-form/Tests/AppTests/AppTests.swift
@@ -1,11 +1,18 @@
 @testable import App
 import Hummingbird
 import HummingbirdTesting
+import Logging
 import XCTest
 
 final class AppTests: XCTestCase {
+    struct TestArguments: AppArguments {
+        var hostname: String = "127.0.0.1"
+        var port = 8080
+        var logLevel: Logger.Level? = .trace
+    }
+
     func testApp() async throws {
-        let app = try await buildApplication(configuration: .init())
+        let app = try await buildApplication(args: TestArguments())
 
         try await app.test(.router) { client in
             let urlencoded = "name=Adam&age=34"
