f4489f24f44ea9e416c39eb900a705925da17627
Adam Fowler <adamfowler71@gmail.com>
2024-06-14 10:28:18 +0100

    Fix up multipart-form

--- multipart-form/Sources/App/RequestContext.swift
+++ multipart-form/Sources/App/RequestContext.swift
@@ -6,7 +6,7 @@ struct MultipartRequestContext: RequestContext {
     var requestDecoder: MultipartRequestDecoder { .init() }
     var coreContext: CoreRequestContext
 
-    init(channel: Channel, logger: Logger) {
-        self.coreContext = .init(allocator: channel.allocator, logger: logger)
+    init(source: Source) {
+        self.coreContext = .init(source: source)
     }
 }
