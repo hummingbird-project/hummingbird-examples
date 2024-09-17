b754013b89b37eaf05985b0b30f829930cd04e1c
Adam Fowler <adamfowler71@gmail.com>
2024-09-17 10:46:29 +0100

    HummingbirdWebSocket 2.0

--- websocket-echo/Package.swift
+++ websocket-echo/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", from: "2.0.0-rc.1"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
     ],
     targets: [
