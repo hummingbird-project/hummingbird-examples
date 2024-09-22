ccbc23b1578058f5c47b0019737188e3c00717ba
Adam Fowler <adamfowler71@gmail.com>
2024-09-23 00:35:21 +0100

    todos-auth-fluent

--- todos-auth-fluent/Sources/App/RequestContext.swift
+++ todos-auth-fluent/Sources/App/RequestContext.swift
@@ -1,15 +1,18 @@
+import Foundation
 import Hummingbird
 import HummingbirdAuth
 import Logging
 import NIOCore
 
-struct TodosAuthRequestContext: AuthRequestContext, RequestContext {
+struct TodosAuthRequestContext: AuthRequestContext, SessionRequestContext, RequestContext {
     var coreContext: CoreRequestContextStorage
     var auth: LoginCache
+    var sessions: SessionContext<UUID>
 
     init(source: Source) {
         self.coreContext = .init(source: source)
         self.auth = .init()
+        self.sessions = .init()
     }
 
     var requestDecoder: TodosAuthRequestDecoder {
