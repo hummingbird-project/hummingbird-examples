24a33691728bcc6849a4180396cbbdc9e9e64583
Michael Redig <mredig@gmail.com>
2024-09-23 21:05:31 -0500

    bump requirements

--- todos-auth-fluent/Package.swift
+++ todos-auth-fluent/Package.swift
@@ -8,11 +8,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0-rc.2"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-fluent.git", from: "2.0.0-beta.2"),
-        .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0-rc.4"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-fluent.git", from: "2.0.0-beta.4"),
+        .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0-rc.1"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.48.5"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
     ],
     targets: [
