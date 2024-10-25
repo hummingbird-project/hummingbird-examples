b95e7258616cdc2efdc9ad50c0968b1957ea5969
Adam Fowler <adamfowler71@gmail.com>
2024-10-25 15:31:59 +0100

    Use hummingbird-auth 2.0.0-rc.5

--- auth-otp/Package.swift
+++ auth-otp/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", branch: "main"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0-rc.5"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-postgres.git", from: "0.5.0"),
         .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
