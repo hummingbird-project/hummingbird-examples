3198e2ecd5a88b76cef75501583a514474b1aeff
Adam Fowler <adamfowler71@gmail.com>
2024-10-25 16:05:47 +0100

    Fix up hummingbird version in todos-auth-fluent

--- todos-auth-fluent/Package.swift
+++ todos-auth-fluent/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v14),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "throwing-request-context-init"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.3.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0-rc.5"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-compression.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-fluent.git", from: "2.0.0-beta.2"),
