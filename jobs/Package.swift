55f5fc15d202ec79c0468ec03190921dda5b93ac
Adam Fowler <adamfowler71@gmail.com>
2025-05-01 09:07:56 +0100

    Update for latest changes in swift-jobs

--- jobs/Package.swift
+++ jobs/Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "jobs",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v14)
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-redis.git", from: "2.0.0-beta.2"),
-        .package(url: "https://github.com/hummingbird-project/swift-jobs.git", branch: "1.0.0-beta.1"),
-        .package(url: "https://github.com/hummingbird-project/swift-jobs-redis.git", branch: "1.0.0-beta.2"),
+        .package(url: "https://github.com/hummingbird-project/swift-jobs.git", from: "1.0.0-beta.8"),
+        .package(url: "https://github.com/hummingbird-project/swift-jobs-redis.git", from: "1.0.0-beta.7"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
     ],
     targets: [
@@ -27,7 +27,7 @@ let package = Package(
                 // Enable better optimizations when building in Release configuration. Despite the use of
                 // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
                 // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
         .testTarget(
