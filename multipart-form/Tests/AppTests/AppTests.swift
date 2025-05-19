43a32b0985e851a500cbdaa1ecb703f2cea102f4
iankoex <hello@iankoex.com>
2025-05-19 21:56:30 +0300

    tests for form data post request and unique file names on disk

--- multipart-form/Tests/AppTests/AppTests.swift
+++ multipart-form/Tests/AppTests/AppTests.swift
@@ -1,9 +1,18 @@
 @testable import App
 import Hummingbird
 import HummingbirdTesting
+import NIOFileSystem
 import XCTest
 
 final class AppTests: XCTestCase {
+
+    override func tearDown() async throws {
+        // Clean up any created directories or files
+        let contentDirectory = try await File.getContentDirectory()
+        try await FileSystem.shared.removeItem(at: contentDirectory)
+        try await super.tearDown()
+    }
+
     struct TestArguments: AppArguments {
         let hostname = "127.0.0.1"
         let port = 8080
@@ -22,7 +31,7 @@ final class AppTests: XCTestCase {
             \r
             50\r
             ------HBTestFormBoundaryXD6BXJI\r
-            Content-Disposition: form-data; name="pfp"; filename="example.txt"\r
+            Content-Disposition: form-data; name="profilePicture"; filename="example.txt"\r
             Content-Type: text/plain\r
             \r
             Hummingbird\r
---
+++ multipart-form/Tests/AppTests/FileHelperTests.swift
@@ -0,0 +1,46 @@
+//
+//  FileHelperTests.swift
+//  multipart-form
+//
+//  Created by ian on 19/05/2025.
+//
+
+@testable import App
+import Hummingbird
+import HummingbirdTesting
+import NIOFileSystem
+import XCTest
+
+final class FileHelperTests: XCTestCase {
+    override func tearDown() async throws {
+        // Clean up any created directories or files
+        let contentDirectory = try await File.getContentDirectory()
+        try await FileSystem.shared.removeItem(at: contentDirectory)
+        try await super.tearDown()
+    }
+
+    func testGetUniqueFilePath() async throws {
+        let byteBuffer = ByteBuffer(data: "this is a test".data(using: .utf8) ?? Data())
+        let testFile = File(
+            data: byteBuffer,
+            filename: "test-file.txt",
+            contentType: "text/plain"
+        )
+
+        let testFileRelativePath = try await testFile.saveDataToDisk()
+        XCTAssertEqual(testFileRelativePath, "content/test-file.txt")
+        let testFileRelativePath1 = try await testFile.saveDataToDisk()
+        XCTAssertEqual(testFileRelativePath1, "content/test-file-1.txt")
+        let testFileRelativePath2 = try await testFile.saveDataToDisk()
+        XCTAssertEqual(testFileRelativePath2, "content/test-file-2.txt")
+
+        let testFile2 = File(
+            data: byteBuffer,
+            filename: "test-file-3.txt",
+            contentType: "text/plain"
+        )
+
+        let testFileRelativePath3 = try await testFile2.saveDataToDisk()
+        XCTAssertEqual(testFileRelativePath3, "content/test-file-3.txt")
+    }
+}
