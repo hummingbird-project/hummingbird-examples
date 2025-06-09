1c6d0a72899c405a555dd7e9cd381df2dd6ea0b0
iankoex <hello@iankoex.com>
2025-06-09 18:05:50 +0300

    handling file upload (#148)
    * handling file upload
    
    * tests passing
    
    * add implementation for saving file to disk and showing image after submitting,
    
    * tests for form data post request and unique file names on disk
    
    * change Bytebuffer init from data to bytes for compatablity with linux
    
    * simpler implementation of uploading and retrieving file
    
    * NIOFileSystem is no longer required
    
    * added documentation and comments
    
    * specify the image type for profile picture and number for age input
    
    ---------
    
    Co-authored-by: Adam Fowler <adamfowler71@gmail.com>

--- multipart-form/Sources/App/Controllers/WebController.swift
+++ multipart-form/Sources/App/Controllers/WebController.swift
@@ -15,39 +15,100 @@
 import Hummingbird
 import Mustache
 
+/// A structure that generates an HTML response.
 struct HTML: ResponseGenerator {
+    /// The HTML content to be included in the response.
     let html: String
 
-    public func response(from request: Request, context: some RequestContext) throws -> Response {
+    /// Generates a response from the given request and context.
+    ///
+    /// - Parameters:
+    ///   - request: The incoming request.
+    ///   - context: The context for the request.
+    /// - Returns: A `Response` object containing the HTML content.
+    public func response(from request: Request, context: some RequestContext) -> Response {
         let buffer = ByteBuffer(string: self.html)
-        return .init(status: .ok, headers: [.contentType: "text/html"], body: .init(byteBuffer: buffer))
+        return Response(status: .ok, headers: [.contentType: "text/html"], body: .init(byteBuffer: buffer))
     }
 }
 
+/// A controller for handling web requests and generating responses.
 struct WebController {
+    /// The Mustache library used for rendering templates.
     let library: MustacheLibrary
+
+    /// The Mustache template for entering user details.
     let enterTemplate: MustacheTemplate
+
+    /// The Mustache template for displaying entered details.
     let enteredTemplate: MustacheTemplate
 
-    init(mustacheLibrary: MustacheLibrary) {
+    /// The virtual file system for storing and retriving files.
+    let fileSystem: VirtualFileSystem
+
+    /// Initializes a new instance of `WebController`.
+    ///
+    /// - Parameters:
+    ///   - mustacheLibrary: The Mustache library for template rendering.
+    ///   - fileSystem: A virtual file system (defaults to a new instance).
+    init(
+        mustacheLibrary: MustacheLibrary,
+        fileSystem: VirtualFileSystem = VirtualFileSystem()
+    ) {
         self.library = mustacheLibrary
         self.enterTemplate = mustacheLibrary.getTemplate(named: "enter-details")!
         self.enteredTemplate = mustacheLibrary.getTemplate(named: "details-entered")!
+        self.fileSystem = fileSystem
     }
 
+    /// Adds routes to the specified router.
     func addRoutes(to router: some RouterMethods<some RequestContext>) {
         router.get("/", use: self.input)
         router.post("/", use: self.post)
+        router.get("files/:filename", use: self.files)
     }
 
+    /// Renders the input form for user details.
     @Sendable func input(request: Request, context: some RequestContext) -> HTML {
         let html = self.enterTemplate.render((), library: self.library)
         return HTML(html: html)
     }
 
+    /// Handles the submission of user details and saves the profile picture.
     @Sendable func post(request: Request, context: some RequestContext) async throws -> HTML {
         let user = try await request.decode(as: User.self, context: context)
-        let html = self.enteredTemplate.render(user, library: self.library)
+        let filename = user.profilePicture.filename
+        let urlSafeFilename = user.profilePicture.urlSafeFilename
+        await fileSystem.save(filename: urlSafeFilename, contents: user.profilePicture)
+
+        let context: [String: Any] = [
+            "name": user.name,
+            "age": user.age,
+            "profilePictureURL": "/files/\(urlSafeFilename)",
+            "profilePictureFilename": filename,
+        ]
+        let html = self.enteredTemplate.render(context, library: self.library)
         return HTML(html: html)
+    }
+
+    /// Retrieves a file from the virtual file system.
+    @Sendable func files(_ request: Request, context: some RequestContext) async throws -> Response {
+        let filename = try context.parameters.require("filename", as: String.self)
+        guard let file = await fileSystem.load(filename: filename) else {
+            throw HTTPError(.notFound, message: "A file with the specified name was not found")
+        }
+        return Response(
+            status: .ok,
+            headers: self.headers(for: file),
+            body: ResponseBody(byteBuffer: file.data)
+        )
+    }
+
+    /// Generates the HTTP headers for a given file.
+    private func headers(for file: File) -> HTTPFields {
+        return [
+            .contentDisposition: "attachment; filename=\"\(file.filename)\"",
+            .contentType: file.contentType,
+        ]
     }
 }
--- multipart-form/Sources/App/Models/User.swift
+++ multipart-form/Sources/App/Models/User.swift
@@ -14,7 +14,14 @@
 
 import Hummingbird
 
-struct User: Codable {
+/// A structure representing a user in the App.
+struct User: Decodable {
+    /// The name of the user.
     let name: String
+
+    /// The age of the user.
     let age: Int
+
+    /// The profile picture of the user, represented as a `File`.
+    let profilePicture: File
 }
