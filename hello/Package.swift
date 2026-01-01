f112177a1a050f0aa9945b07bf497b1d53ce7637
Adam Fowler <adamfowler71@gmail.com>
2026-01-01 19:32:28 +0100

    Update hello example to use output from latest template

--- hello/Package.swift
+++ hello/Package.swift
@@ -1,36 +1,32 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "hello",
-    platforms: [
-        .macOS(.v14),
+    platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18)],
+    products: [
+        .executable(name: "App", targets: ["App"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.0.0", traits: [.defaults, "CommandLineArguments"]),
     ],
     targets: [
-        .executableTarget(
-            name: "App",
+        .executableTarget(name: "App",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Configuration", package: "swift-configuration"),
                 .product(name: "Hummingbird", package: "hummingbird"),
             ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
-            ]
+            path: "Sources/App"
         ),
-        .testTarget(
-            name: "AppTests",
+        .testTarget(name: "AppTests",
             dependencies: [
                 .byName(name: "App"),
-                .product(name: "Hummingbird", package: "hummingbird"),
-                .product(name: "HummingbirdTesting", package: "hummingbird"),
-            ]
-        ),
+                .product(name: "HummingbirdTesting", package: "hummingbird")
+            ],
+            path: "Tests/AppTests"
+        )
     ]
 )
--- hello/README.md
+++ hello/README.md
@@ -1,9 +1,9 @@
 # Hello
 
-This is a simple app that will responds with the word "hello". This example is here just to demonstrate how to start up a server.
+This is a simple app that will responds with the string "Hello!". This example is here just to demonstrate how to start up a server. The example code is exactly the same as the output from the template when you chose all the default options.
 
 You can test the sample as follows
 ```sh
-curl localhost:8080/
+curl -i localhost:8080/
 ```
-It should return a response with status 200 and the body text "Hello".
+It should return a response with status 200 and the body text "Hello!".
