aad9ac54f0dc5431d139a012916eed65175ca2b6
Adam Fowler <adamfowler71@gmail.com>
2026-01-05 19:20:47 +0100

    Update tutorial with swift-configuration and testing

--- todos-postgres-tutorial/Package.swift
+++ todos-postgres-tutorial/Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "todos-postgres-tutorial",
-    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
+    name: "Todos",
+    platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18)],
     products: [
         .executable(name: "App", targets: ["App"])
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.0.0", traits: [.defaults, "CommandLineArguments"]),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.21.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Configuration", package: "swift-configuration"),
                 .product(name: "Hummingbird", package: "hummingbird"),
                 .product(name: "PostgresNIO", package: "postgres-nio"),
             ],
--- todos-postgres-tutorial/Tests/AppTests/AppTests.swift
+++ todos-postgres-tutorial/Tests/AppTests/AppTests.swift
@@ -1,3 +1,4 @@
+import Configuration
 import Foundation
 import Hummingbird
 import HummingbirdTesting
@@ -6,20 +7,23 @@ import Testing
 
 @testable import App
 
-struct AppTests {
-    struct TestArguments: AppArguments {
-        let hostname = "127.0.0.1"
-        let port = 8080
-        let logLevel: Logger.Level? = nil
-        let inMemoryTesting = true
-    }
+private let reader = ConfigReader(providers: [
+    InMemoryProvider(values: [
+        "host": "127.0.0.1",
+        "port": "0",
+        "log.level": "trace",
+        "db.inMemoryTesting": true,
+    ])
+])
 
+@Suite
+struct AppTests {
     struct CreateRequest: Encodable {
         let title: String
         let order: Int?
     }
 
-    static func create(title: String, order: Int? = nil, client: some TestClientProtocol) async throws -> Todo {
+    func create(title: String, order: Int? = nil, client: some TestClientProtocol) async throws -> Todo {
         let request = CreateRequest(title: title, order: order)
         let buffer = try JSONEncoder().encodeAsByteBuffer(request, allocator: ByteBufferAllocator())
         return try await client.execute(uri: "/todos", method: .post, body: buffer) { response in
@@ -28,7 +32,7 @@ struct AppTests {
         }
     }
 
-    static func get(id: UUID, client: some TestClientProtocol) async throws -> Todo? {
+    func get(id: UUID, client: some TestClientProtocol) async throws -> Todo? {
         try await client.execute(uri: "/todos/\(id)", method: .get) { response in
             // either the get request returned an 200 status or it didn't return a Todo
             #expect(response.status == .ok || response.body.readableBytes == 0)
@@ -40,7 +44,7 @@ struct AppTests {
         }
     }
 
-    static func list(client: some TestClientProtocol) async throws -> [Todo] {
+    func list(client: some TestClientProtocol) async throws -> [Todo] {
         try await client.execute(uri: "/todos", method: .get) { response in
             #expect(response.status == .ok)
             return try JSONDecoder().decode([Todo].self, from: response.body)
@@ -53,7 +57,7 @@ struct AppTests {
         let completed: Bool?
     }
 
-    static func patch(
+    func patch(
         id: UUID,
         title: String? = nil,
         order: Int? = nil,
@@ -72,90 +76,90 @@ struct AppTests {
         }
     }
 
-    static func delete(id: UUID, client: some TestClientProtocol) async throws -> HTTPResponse.Status {
+    func delete(id: UUID, client: some TestClientProtocol) async throws -> HTTPResponse.Status {
         try await client.execute(uri: "/todos/\(id)", method: .delete) { response in
             response.status
         }
     }
 
-    static func deleteAll(client: some TestClientProtocol) async throws {
+    func deleteAll(client: some TestClientProtocol) async throws {
         try await client.execute(uri: "/todos", method: .delete) { _ in }
     }
 
     // MARK: Tests
 
     @Test func testCreate() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
-            let todo = try await Self.create(title: "My first todo", client: client)
+            let todo = try await self.create(title: "My first todo", client: client)
             #expect(todo.title == "My first todo")
         }
     }
 
     @Test func testPatch() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
             // create todo
-            let todo = try await Self.create(title: "Deliver parcels to James", client: client)
+            let todo = try await self.create(title: "Deliver parcels to James", client: client)
             // rename it
-            _ = try await Self.patch(id: todo.id, title: "Deliver parcels to Claire", client: client)
-            let editedTodo = try await Self.get(id: todo.id, client: client)
+            _ = try await self.patch(id: todo.id, title: "Deliver parcels to Claire", client: client)
+            let editedTodo = try await self.get(id: todo.id, client: client)
             #expect(editedTodo?.title == "Deliver parcels to Claire")
             // set it to completed
-            _ = try await Self.patch(id: todo.id, completed: true, client: client)
-            let editedTodo2 = try await Self.get(id: todo.id, client: client)
+            _ = try await self.patch(id: todo.id, completed: true, client: client)
+            let editedTodo2 = try await self.get(id: todo.id, client: client)
             #expect(editedTodo2?.completed == true)
             // revert it
-            _ = try await Self.patch(id: todo.id, title: "Deliver parcels to James", completed: false, client: client)
-            let editedTodo3 = try await Self.get(id: todo.id, client: client)
+            _ = try await self.patch(id: todo.id, title: "Deliver parcels to James", completed: false, client: client)
+            let editedTodo3 = try await self.get(id: todo.id, client: client)
             #expect(editedTodo3?.title == "Deliver parcels to James")
             #expect(editedTodo3?.completed == false)
         }
     }
 
     @Test func testAPI() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
             // create two todos
-            let todo1 = try await Self.create(title: "Wash my hair", client: client)
-            let todo2 = try await Self.create(title: "Brush my teeth", client: client)
+            let todo1 = try await self.create(title: "Wash my hair", client: client)
+            let todo2 = try await self.create(title: "Brush my teeth", client: client)
             // get first todo
-            let getTodo = try await Self.get(id: todo1.id, client: client)
+            let getTodo = try await self.get(id: todo1.id, client: client)
             #expect(getTodo == todo1)
             // patch second todo
-            let optionalPatchedTodo = try await Self.patch(id: todo2.id, completed: true, client: client)
+            let optionalPatchedTodo = try await self.patch(id: todo2.id, completed: true, client: client)
             let patchedTodo = try #require(optionalPatchedTodo)
             #expect(patchedTodo.completed == true)
             #expect(patchedTodo.title == todo2.title)
             // get all todos and check first todo and patched second todo are in the list
-            let todos = try await Self.list(client: client)
+            let todos = try await self.list(client: client)
             #expect(todos.firstIndex(of: todo1) != nil)
             #expect(todos.firstIndex(of: patchedTodo) != nil)
             // delete a todo and verify it has been deleted
-            let status = try await Self.delete(id: todo1.id, client: client)
+            let status = try await self.delete(id: todo1.id, client: client)
             #expect(status == .ok)
-            let deletedTodo = try await Self.get(id: todo1.id, client: client)
+            let deletedTodo = try await self.get(id: todo1.id, client: client)
             #expect(deletedTodo == nil)
             // delete all todos and verify there are none left
-            try await Self.deleteAll(client: client)
-            let todos2 = try await Self.list(client: client)
+            try await self.deleteAll(client: client)
+            let todos2 = try await self.list(client: client)
             #expect(todos2.count == 0)
         }
     }
 
     @Test func testDeletingTodoTwiceReturnsBadRequest() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
-            let todo = try await Self.create(title: "Delete me", client: client)
-            let status1 = try await Self.delete(id: todo.id, client: client)
+            let todo = try await self.create(title: "Delete me", client: client)
+            let status1 = try await self.delete(id: todo.id, client: client)
             #expect(status1 == .ok)
-            let status2 = try await Self.delete(id: todo.id, client: client)
+            let status2 = try await self.delete(id: todo.id, client: client)
             #expect(status2 == .badRequest)
         }
     }
 
     @Test func testGettingTodoWithInvalidUUIDReturnsBadRequest() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
             // The get helper function doesnt allow me to supply random strings
             try await client.execute(uri: "/todos/NotAUUID", method: .get) { response in
@@ -165,12 +169,12 @@ struct AppTests {
     }
 
     @Test func test30ConcurrentlyCreatedTodosAreAllCreated() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
             let todos = try await withThrowingTaskGroup(of: Todo.self) { group in
                 for count in 0..<30 {
                     group.addTask {
-                        try await Self.create(title: "Todo: \(count)", client: client)
+                        try await self.create(title: "Todo: \(count)", client: client)
                     }
                 }
                 var todos: [Todo] = []
@@ -179,7 +183,7 @@ struct AppTests {
                 }
                 return todos
             }
-            let todoList = try await Self.list(client: client)
+            let todoList = try await self.list(client: client)
             for todo in todos {
                 #expect(todoList.firstIndex(of: todo) != nil)
             }
@@ -187,7 +191,7 @@ struct AppTests {
     }
 
     @Test func testUpdatingNonExistentTodoReturnsBadRequest() async throws {
-        let app = try await buildApplication(TestArguments())
+        let app = try await buildApplication(reader: reader)
         try await app.test(.router) { client in
             // The patch helper function assumes it is going to work so we have to write our own here
             let request = UpdateRequest(title: "Update", order: nil, completed: nil)
