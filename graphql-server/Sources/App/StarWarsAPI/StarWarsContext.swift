375e830016fbba3b69227219905451c8b0581a4f
Adam Fowler <adamfowler71@gmail.com>
2026-01-13 11:53:10 +0000

    Update graphql-server for swift 6 and Grafiti v3

--- graphql-server/Sources/App/StarWarsAPI/StarWarsContext.swift
+++ graphql-server/Sources/App/StarWarsAPI/StarWarsContext.swift
@@ -1,20 +1,113 @@
-/**
- * StarWars API Example
- * Graphiti
- * This example comes from the [Graphiti StarWarsAPI](https://github.com/GraphQLSwift/Graphiti/tree/main/Tests/GraphitiTests/StarWarsAPI) example
- *
- * The Graphiti [README](https://github.com/GraphQLSwift/Graphiti#getting-started) is also a helpful reference.
- */
-/**
- * This defines a basic set of data for our Star Wars Schema.
- *
- * This data is hard coded for the sake of the demo, but you could imagine
- * fetching this data from a backend service rather than from hardcoded
- * values in a more complex demo.
- */
-import Hummingbird
+/// StarWars API Example
+/// Graphiti
+/// This example comes from the [Graphiti StarWarsAPI](https://github.com/GraphQLSwift/Graphiti/tree/main/Tests/GraphitiTests/StarWarsAPI) example
+///
+/// The Graphiti [README](https://github.com/GraphQLSwift/Graphiti#getting-started) is also a helpful reference.
+/// This defines a basic set of data for our Star Wars Schema.
+///
+/// This data is hard coded for the sake of the demo, but you could imagine
+/// fetching this data from a backend service rather than from hardcoded
+/// values in a more complex demo.
+/// This defines a basic set of data for our Star Wars Schema.
+///
+/// This data is hard coded for the sake of the demo, but you could imagine
+/// fetching this data from a backend service rather than from hardcoded
+/// values in a more complex demo.
+public final class StarWarsContext: Sendable {
+    private static let tatooine = Planet(
+        id: "10001",
+        name: "Tatooine",
+        diameter: 10465,
+        rotationPeriod: 23,
+        orbitalPeriod: 304,
+        residents: []
+    )
 
-public final class StarWarsContext {
+    private static let alderaan = Planet(
+        id: "10002",
+        name: "Alderaan",
+        diameter: 12500,
+        rotationPeriod: 24,
+        orbitalPeriod: 364,
+        residents: []
+    )
+
+    private static let planetData: [String: Planet] = [
+        "10001": tatooine,
+        "10002": alderaan,
+    ]
+
+    private static let luke = Human(
+        id: "1000",
+        name: "Luke Skywalker",
+        friends: ["1002", "1003", "2000", "2001"],
+        appearsIn: [.newHope, .empire, .jedi],
+        homePlanet: tatooine
+    )
+
+    private static let vader = Human(
+        id: "1001",
+        name: "Darth Vader",
+        friends: ["1004"],
+        appearsIn: [.newHope, .empire, .jedi],
+        homePlanet: tatooine
+    )
+
+    private static let han = Human(
+        id: "1002",
+        name: "Han Solo",
+        friends: ["1000", "1003", "2001"],
+        appearsIn: [.newHope, .empire, .jedi],
+        homePlanet: alderaan
+    )
+
+    private static let leia = Human(
+        id: "1003",
+        name: "Leia Organa",
+        friends: ["1000", "1002", "2000", "2001"],
+        appearsIn: [.newHope, .empire, .jedi],
+        homePlanet: alderaan
+    )
+
+    private static let tarkin = Human(
+        id: "1004",
+        name: "Wilhuff Tarkin",
+        friends: ["1001"],
+        appearsIn: [.newHope],
+        homePlanet: alderaan
+    )
+
+    private static let humanData: [String: Human] = [
+        "1000": luke,
+        "1001": vader,
+        "1002": han,
+        "1003": leia,
+        "1004": tarkin,
+    ]
+
+    private static let c3po = Droid(
+        id: "2000",
+        name: "C-3PO",
+        friends: ["1000", "1002", "1003", "2001"],
+        appearsIn: [.newHope, .empire, .jedi],
+        primaryFunction: "Protocol"
+    )
+
+    private static let r2d2 = Droid(
+        id: "2001",
+        name: "R2-D2",
+        friends: ["1000", "1002", "1003"],
+        appearsIn: [.newHope, .empire, .jedi],
+        primaryFunction: "Astromech"
+    )
+
+    private static let droidData: [String: Droid] = [
+        "2000": c3po,
+        "2001": r2d2,
+    ]
+
+    public init() {}
+
     /**
      * Helper function to get a character by ID.
      */
@@ -25,16 +118,16 @@ public final class StarWarsContext {
     /**
      * Allows us to query for a character"s friends.
      */
-    public func getFriends(of character: Character) async throws -> [Character] {
+    public func getFriends(of character: Character) -> [Character] {
         character.friends.compactMap { id in
-            self.getCharacter(id: id)
+            getCharacter(id: id)
         }
     }
 
     /**
      * Allows us to fetch the undisputed hero of the Star Wars trilogy, R2-D2.
      */
-    public func getHero(of episode: Episode?) async throws -> Character {
+    public func getHero(of episode: Episode?) -> Character {
         if episode == .empire {
             // Luke is the hero of Episode V.
             return Self.luke
@@ -46,21 +139,21 @@ public final class StarWarsContext {
     /**
      * Allows us to query for the human with the given id.
      */
-    public func getHuman(id: String) async throws -> Human? {
+    public func getHuman(id: String) -> Human? {
         Self.humanData[id]
     }
 
     /**
      * Allows us to query for the droid with the given id.
      */
-    public func getDroid(id: String) async throws -> Droid? {
+    public func getDroid(id: String) -> Droid? {
         Self.droidData[id]
     }
 
     /**
      * Allows us to get the secret backstory, or not.
      */
-    public func getSecretBackStory() async throws -> String? {
+    public func getSecretBackStory() throws -> String? {
         struct Secret: Error, CustomStringConvertible {
             let description: String
         }
@@ -71,7 +164,7 @@ public final class StarWarsContext {
     /**
      * Allows us to query for a Planet.
      */
-    public func getPlanets(query: String) async throws -> [Planet] {
+    public func getPlanets(query: String) -> [Planet] {
         Self.planetData
             .sorted(by: { $0.key < $1.key })
             .map { $1 }
@@ -81,7 +174,7 @@ public final class StarWarsContext {
     /**
      * Allows us to query for a Human.
      */
-    public func getHumans(query: String) async throws -> [Human] {
+    public func getHumans(query: String) -> [Human] {
         Self.humanData
             .sorted(by: { $0.key < $1.key })
             .map { $1 }
@@ -91,7 +184,7 @@ public final class StarWarsContext {
     /**
      * Allows us to query for a Droid.
      */
-    public func getDroids(query: String) async throws -> [Droid] {
+    public func getDroids(query: String) -> [Droid] {
         Self.droidData
             .sorted(by: { $0.key < $1.key })
             .map { $1 }
@@ -101,102 +194,7 @@ public final class StarWarsContext {
     /**
      * Allows us to query for either a Human, Droid, or Planet.
      */
-    public func search(query: String) async throws -> [SearchResult] {
-        let planets = try await self.getPlanets(query: query)
-        let humans = try await self.getHumans(query: query)
-        let droids = try await self.getDroids(query: query)
-        return .init() + planets + humans + droids
+    public func search(query: String) -> [SearchResult] {
+        getPlanets(query: query) + getHumans(query: query) + getDroids(query: query)
     }
-
-    private static var tatooine = Planet(
-        id: "10001",
-        name: "Tatooine",
-        diameter: 10465,
-        rotationPeriod: 23,
-        orbitalPeriod: 304,
-        residents: []
-    )
-
-    private static var alderaan = Planet(
-        id: "10002",
-        name: "Alderaan",
-        diameter: 12500,
-        rotationPeriod: 24,
-        orbitalPeriod: 364,
-        residents: []
-    )
-
-    private static var planetData: [String: Planet] = [
-        "10001": tatooine,
-        "10002": alderaan,
-    ]
-
-    private static var luke = Human(
-        id: "1000",
-        name: "Luke Skywalker",
-        friends: ["1002", "1003", "2000", "2001"],
-        appearsIn: [.newHope, .empire, .jedi],
-        homePlanet: tatooine
-    )
-
-    private static var vader = Human(
-        id: "1001",
-        name: "Darth Vader",
-        friends: ["1004"],
-        appearsIn: [.newHope, .empire, .jedi],
-        homePlanet: tatooine
-    )
-
-    private static var han = Human(
-        id: "1002",
-        name: "Han Solo",
-        friends: ["1000", "1003", "2001"],
-        appearsIn: [.newHope, .empire, .jedi],
-        homePlanet: alderaan
-    )
-
-    private static var leia = Human(
-        id: "1003",
-        name: "Leia Organa",
-        friends: ["1000", "1002", "2000", "2001"],
-        appearsIn: [.newHope, .empire, .jedi],
-        homePlanet: alderaan
-    )
-
-    private static var tarkin = Human(
-        id: "1004",
-        name: "Wilhuff Tarkin",
-        friends: ["1001"],
-        appearsIn: [.newHope],
-        homePlanet: alderaan
-    )
-
-    private static var humanData: [String: Human] = [
-        "1000": luke,
-        "1001": vader,
-        "1002": han,
-        "1003": leia,
-        "1004": tarkin,
-    ]
-
-    private static var c3po = Droid(
-        id: "2000",
-        name: "C-3PO",
-        friends: ["1000", "1002", "1003", "2001"],
-        appearsIn: [.newHope, .empire, .jedi],
-        primaryFunction: "Protocol"
-    )
-
-    private static var r2d2 = Droid(
-        id: "2001",
-        name: "R2-D2",
-        friends: ["1000", "1002", "1003"],
-        appearsIn: [.newHope, .empire, .jedi],
-        primaryFunction: "Astromech"
-    )
-
-    private static var droidData: [String: Droid] = [
-        "2000": c3po,
-        "2001": r2d2,
-    ]
 }
