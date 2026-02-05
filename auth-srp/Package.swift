d0497cde05ee59b4ac226c94e5bedbfd2dfcc62c
Adam Fowler <adamfowler71@gmail.com>
2026-02-05 14:30:23 +0000

    Swift 6 for auth-srp (#166)

--- auth-srp/Package.swift
+++ auth-srp/Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "auth-srp",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v14)
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.19.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-fluent.git", from: "2.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.8.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.0"),
         .package(url: "https://github.com/adam-fowler/swift-srp.git", from: "2.1.0"),
@@ -31,7 +31,7 @@ let package = Package(
                 // Enable better optimizations when building in Release configuration. Despite the use of
                 // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
                 // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
         .testTarget(
