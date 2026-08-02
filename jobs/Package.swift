b19f1f1b75d9cbfaa7f743d904ebeee9e369e6e7
Adam Fowler <adamfowler71@gmail.com>
2026-08-02 07:19:09 +0100

    jobs: Use swift-jobs-valkey 1.0.0

--- jobs/Package.swift
+++ jobs/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.19.0"),
         .package(url: "https://github.com/hummingbird-project/swift-jobs.git", from: "1.3.0"),
-        .package(url: "https://github.com/hummingbird-project/swift-jobs-valkey.git", branch: "1.0-cleanup"),
+        .package(url: "https://github.com/hummingbird-project/swift-jobs-valkey.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
     ],
     targets: [
